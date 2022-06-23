8c128421fb566d59a4212ca69eb1e1263fc74b6e
RakeshSuvvari <rakeshsuvvari3@gmail.com>
2022-06-23 16:52:14 +0530

    add

---
+++ practice.js
@@ -0,0 +1,29 @@
+console.log(a)
+console.log(b)
+
+var a = 10
+var b = 20
+
+simple()
+
+function simple(){
+    console.log("This is a function")
+}
+
+console.log(undefined, typeof undefined)
+console.log(1, typeof 1)
+
+var x
+console.log(x)
+
+a = 100
+// console.log(a)
+
+function getmessage(){
+    return "Javascript is asynchronous"
+}
+
+message = getmessage()
+
+console.log(message)
+// confirm()
